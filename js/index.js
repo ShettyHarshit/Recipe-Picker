7874a02a2b3c9ec8c6d93fe3d135bf0cd60e20b5
Harshit Shetty <swaharsh@gmail.com>
2017-09-18 22:49:01 +0530

    Recipe/Ingredients Tabs created

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ app.controller("mah-controller", function($scope) {
     "Name" : "Chicken Pasta",
     "Instructions" : "Step 1: Preheat oven to 200°C. Cook the penne in a large saucepan of boiling water following packet directions or until al dente. Drain well. Bake for 20 mins or until top is golden brown and pasta mixture is heated through. Serve immediately.",
     "Ingredients" : ["Chicken", "Pasta", "Cheese"],
-    "Link" : "http://www.meckmin.org/wp-content/uploads/2015/08/person-placeholder-608x608.jpg"
+    "Link" : "https://supervalu.ie/thumbnail/720x400/var/files/good-food-karma/recipe/48423/MainPasta.jpg?fill=1",
+    "List" : "100 gm penne pasta, 1 cup cherry tomatoes, halved, ½ cup smoked chicken, 2 Tbsp parmesan, grated, Salt to taste, A bunch of fresh basil leaves, 2-3 garlic cloves, 1 Tbsp pine nuts, ¼ cup Parmesan, ½ cup olive oil"
 },{
     "Name" : "Brocolli Beef salad",
     "Instructions" : "20",
